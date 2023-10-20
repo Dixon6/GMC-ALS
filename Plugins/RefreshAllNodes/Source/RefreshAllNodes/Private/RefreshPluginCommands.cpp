881a6bf17ee982d869ae1014e60518567ccea5e3
Dixon <103589964+Dixon6@users.noreply.github.com>
2023-10-19 22:33:35 -0400

    Remove RefreshNodesPlugin

--- Plugins/RefreshAllNodes/Source/RefreshAllNodes/Private/RefreshPluginCommands.cpp
+++
@@ -1,9 +0,0 @@
-#include "RefreshPluginCommands.h"
-#include "RefreshAllNodes.h"
-
-void FRefreshPluginCommands::RegisterCommands() {
-#define LOCTEXT_NAMESPACE ""
-	UI_COMMAND(RefreshAllButton, "Refresh All Blueprint Nodes", "Refresh all nodes in every blueprint", EUserInterfaceActionType::Button, FInputChord());
-	UI_COMMAND(RefreshPathButton, "Refresh Blueprints", "Refresh all nodes in blueprints under this folder", EUserInterfaceActionType::Button, FInputChord());
-#undef LOCTEXT_NAMESPACE
-}
